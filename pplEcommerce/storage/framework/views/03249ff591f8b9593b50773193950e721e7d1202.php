386ecc603f767baa47771d41c8cb569e3d139627
perdanaph <danangputro@gmail.com>
2022-05-21 18:05:40 +0700

    Menambah Project Laravel

---
+++ pplEcommerce/storage/framework/views/03249ff591f8b9593b50773193950e721e7d1202.php
@@ -0,0 +1,4 @@
+<div>
+<h1>User dashboard</h1>
+</div>
+<?php /**PATH /opt/lampp/htdocs/pplEcommerce/resources/views/livewire/user/user-dashboard-component.blade.php ENDPATH**/ ?>
---
+++ pplEcommerce/storage/framework/views/e0a19b3f4c7f1173235f701fe3c40c6768dbad54.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/animate.css')); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/font-awesome.min.css')); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/bootstrap.min.css')); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/owl.carousel.min.css')); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/chosen.min.css')); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/style.css')); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/color-01.css')); ?>">
+	<title>Home</title>
+</head>
+<body class="home-page home-01 ">
+
+	<!-- mobile menu -->
+    <div class="mercado-clone-wrap">
+        <div class="mercado-panels-actions-wrap">
+            <a class="mercado-close-btn mercado-close-panels" href="#">x</a>
+        </div>
+        <div class="mercado-panels"></div>
+    </div>
+
+	<!--header-->
+	<header id="header" class="header header-style-1">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="topbar-menu-area">
+					<div class="container">
+						<div class="topbar-menu right-menu">
+							<ul>
+								<li class="menu-item lang-menu menu-item-has-children parent">
+                                <?php if(Route::has('login')): ?>
+                                    <?php if(auth()->guard()->check()): ?>
+                                        <?php if(Auth::user()->user_type === "ADM"): ?>
+											<li class="menu-item menu-item-has-children parent" >
+												<a title="My Account" href="">My Account (<?php echo e(Auth::user()->name); ?>)<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+												<ul class="submenu curency" >
+													<li class="menu-item" >
+														<a title="dashboard" href="<?php echo e(route('admin.dashboard')); ?>">Dashboard</a>
+													</li>
+                                                    <li class="menu-item">
+                                                        <a href="<?php echo e(route('logout')); ?>" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                                    </li>
+                                                    <form id="logout-form" method="POST" action="<?php echo e(route('logout')); ?>">
+                                                        <?php echo csrf_field(); ?>
+                                                    </form>
+												</ul>
+											</li>
+										<?php else: ?>
+											<li class="menu-item menu-item-has-children parent" >
+												<a title="My Account" href="">My Account (<?php echo e(Auth::user()->name); ?>)<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+												<ul class="submenu curency" >
+													<li class="menu-item" >
+														<a title="dashboard" href="<?php echo e(route('user.dashboard')); ?>">Dashboard</a>
+													</li>
+                                                    <li class="menu-item">
+                                                        <a href="<?php echo e(route('logout')); ?>" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                                    </li>
+                                                    <form id="logout-form" method="POST" action="<?php echo e(route('logout')); ?>">
+                                                        <?php echo csrf_field(); ?>
+                                                    </form>
+												</ul>
+											</li>
+                                        <?php endif; ?>
+                                    <?php else: ?>
+                                        <li class="menu-item" ><a title="Register or Login" href="<?php echo e(route('login')); ?>">Login</a></li>
+								        <li class="menu-item" ><a title="Register or Login" href="<?php echo e(route('register')); ?>">Register</a></li>
+                                    <?php endif; ?>
+
+                                <?php endif; ?>
+
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="container">
+					<div class="mid-section main-info-area">
+
+						<div class="wrap-logo-top left-section">
+							<a href="index.html" class="link-to-home"><img src="assets/images/logo-top-1.png" alt="mercado"></a>
+						</div>
+
+						<div class="wrap-search center-section">
+							<div class="wrap-search-form">
+								<form action="#" id="form-search-top" name="form-search-top">
+									<input type="text" name="search" value="" placeholder="Search here...">
+									<button form="form-search-top" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+									<div class="wrap-list-cate">
+										<input type="hidden" name="product-cate" value="0" id="product-cate">
+										<a href="#" class="link-control">All Category</a>
+										<ul class="list-cate">
+											<li class="level-0">All Category</li>
+											<li class="level-1">Gayo</li>
+											<li class="level-1">Temanggung</li>
+											<li class="level-1">Naus</li>
+											<li class="level-1">Kasturi</li>
+										</ul>
+									</div>
+								</form>
+							</div>
+						</div>
+
+						<div class="wrap-icon right-section">
+							<div class="wrap-icon-section minicart">
+								<a href="#" class="link-direction">
+									<i class="fa fa-shopping-basket" aria-hidden="true"></i>
+									<div class="left-info">
+										<span class="index">4 items</span>
+										<span class="title">KERANJANG</span>
+									</div>
+								</a>
+							</div>
+							<div class="wrap-icon-section show-up-after-1024">
+								<a href="#" class="mobile-navigation">
+									<span></span>
+									<span></span>
+									<span></span>
+								</a>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+				<div class="nav-section header-sticky">
+
+					<div class="primary-nav-section">
+						<div class="container">
+							<ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu" >
+								<li class="menu-item home-icon">
+									<a href="/" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
+								</li>
+								<li class="menu-item">
+									<a href="about-us.html" class="link-term mercado-item-title">About Us</a>
+								</li>
+								<li class="menu-item">
+									<a href="/shop" class="link-term mercado-item-title">Shop</a>
+								</li>
+								<li class="menu-item">
+									<a href="/cart" class="link-term mercado-item-title">Cart</a>
+								</li>
+								<li class="menu-item">
+									<a href="/checkout" class="link-term mercado-item-title">Checkout</a>
+								</li>
+								<li class="menu-item">
+									<a href="contact-us.html" class="link-term mercado-item-title">Contact Us</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+    <?php echo e($slot); ?>
+
+
+	<footer id="footer">
+		<div class="wrap-footer-content footer-style-1">
+
+			<div class="wrap-function-info">
+				<div class="container">
+					<ul>
+						<li class="fc-info-item">
+							<i class="fa fa-truck" aria-hidden="true"></i>
+							<div class="wrap-left-info">
+								<h4 class="fc-name">Free Shipping</h4>
+								<p class="fc-desc">Free On Oder Over $99</p>
+							</div>
+
+						</li>
+						<li class="fc-info-item">
+							<i class="fa fa-recycle" aria-hidden="true"></i>
+							<div class="wrap-left-info">
+								<h4 class="fc-name">Guarantee</h4>
+								<p class="fc-desc">30 Days Money Back</p>
+							</div>
+
+						</li>
+						<li class="fc-info-item">
+							<i class="fa fa-credit-card-alt" aria-hidden="true"></i>
+							<div class="wrap-left-info">
+								<h4 class="fc-name">Safe Payment</h4>
+								<p class="fc-desc">Safe your online payment</p>
+							</div>
+
+						</li>
+						<li class="fc-info-item">
+							<i class="fa fa-life-ring" aria-hidden="true"></i>
+							<div class="wrap-left-info">
+								<h4 class="fc-name">Online Suport</h4>
+								<p class="fc-desc">We Have Support 24/7</p>
+							</div>
+
+						</li>
+					</ul>
+				</div>
+			</div>
+			<!--End function info-->
+
+			<div class="main-footer-content">
+
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+							<div class="wrap-footer-item">
+								<h3 class="item-header">Contact Details</h3>
+								<div class="item-content">
+									<div class="wrap-contact-detail">
+										<ul>
+											<li>
+												<i class="fa fa-map-marker" aria-hidden="true"></i>
+												<p class="contact-txt">45 Grand Central Terminal New York,NY 1017 United State USA</p>
+											</li>
+											<li>
+												<i class="fa fa-phone" aria-hidden="true"></i>
+												<p class="contact-txt">(+123) 456 789 - (+123) 666 888</p>
+											</li>
+											<li>
+												<i class="fa fa-envelope" aria-hidden="true"></i>
+												<p class="contact-txt">Contact@yourcompany.com</p>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+
+							<div class="wrap-footer-item">
+								<h3 class="item-header">Hot Line</h3>
+								<div class="item-content">
+									<div class="wrap-hotline-footer">
+										<span class="desc">Call Us toll Free</span>
+										<b class="phone-number">(+123) 456 789 - (+123) 666 888</b>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+						<div class="col-lg-4 col-sm-4 col-md-4 col-xs-12 box-twin-content ">
+							<div class="row">
+								<div class="wrap-footer-item twin-item">
+									<h3 class="item-header">Infomation</h3>
+									<div class="item-content">
+										<div class="wrap-vertical-nav">
+											<ul>
+												<li class="menu-item"><a href="#" class="link-term">Contact Us</a></li>
+												<li class="menu-item"><a href="#" class="link-term">Returns</a></li>
+												<li class="menu-item"><a href="#" class="link-term">Site Map</a></li>
+												<li class="menu-item"><a href="#" class="link-term">Specials</a></li>
+												<li class="menu-item"><a href="#" class="link-term">Order History</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="coppy-right-box">
+				<div class="container">
+					<div class="coppy-right-item item-left">
+						<p class="coppy-right-text">Copyright © 2022 E-Mbako. All rights reserved</p>
+					</div>
+					<div class="coppy-right-item item-right">
+						<div class="wrap-nav horizontal-nav">
+							<ul>
+								<li class="menu-item"><a href="about-us.html" class="link-term">About us</a></li>
+								<li class="menu-item"><a href="privacy-policy.html" class="link-term">Privacy Policy</a></li>
+								<li class="menu-item"><a href="terms-conditions.html" class="link-term">Terms & Conditions</a></li>
+								<li class="menu-item"><a href="return-policy.html" class="link-term">Return Policy</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<script src="<?php echo e(asset('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/bootstrap.min.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/jquery.flexslider.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/chosen.jquery.min.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/owl.carousel.min.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/jquery.countdown.min.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/jquery.sticky.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/functions.js')); ?>"></script>
+</body>
+</html>
+<?php /**PATH /opt/lampp/htdocs/pplEcommerce/resources/views/layouts/base.blade.php ENDPATH**/ ?>
